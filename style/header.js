4f67d38cff95cb5a9cee607da56465a7872dd5f8
David Coelho <davidcoelho.dc@gmail.com>
2025-08-03 09:25:20 +0100

    3.2 reverse

--- style/header.js
+++ style/header.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
   var mouseX, mouseY;
+  var ww = $( window ).width();
+  var wh = $( window ).height();
   var traX, traY;
-
   $(document).mousemove(function(e){
     mouseX = e.pageX;
     mouseY = e.pageY;
     traX = ((4 * mouseX) / 570) + 40;
     traY = ((4 * mouseY) / 570) + 50;
-    console.log(traX + "% " + traY + "%");
-    $(".title-header").css({"background-position": traX + "% " + traY + "%"});
+    console.log(traX);
+    $(".title-header").css({"background-position": traX + "%" + traY + "%"});
   });
 });
